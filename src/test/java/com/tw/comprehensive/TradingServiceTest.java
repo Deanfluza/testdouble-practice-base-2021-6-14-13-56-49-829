47031ad097a479174715dc75685a8fe29fda9f13
dean <deanms@qq.com>
2021-06-16 19:45:44 +0800

    Add Test for Task 1 [Check if logNewTrade() of auditService called when execute createTrade() of TradeService]

--- src/test/java/com/tw/comprehensive/TradingServiceTest.java
+++ src/test/java/com/tw/comprehensive/TradingServiceTest.java
@@ -1,7 +1,24 @@
 package com.tw.comprehensive;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class TradingServiceTest {
+    @Test
+    void should_log_new_trade_been_call_when_call_create_trade() {
 
+        //given
+        TradeRepository tradeRepository = mock(TradeRepository.class);
+        AuditService auditService = mock(AuditService.class);
+        Trade trade = mock(Trade.class);
+        TradingService tradingService = new TradingService(tradeRepository, auditService);
+
+        //when
+        tradingService.createTrade(trade);
+
+        //then
+        verify(auditService, times(1)).logNewTrade(trade);
+    }
 }
