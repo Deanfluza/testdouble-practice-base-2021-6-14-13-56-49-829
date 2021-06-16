6eb3cf3291d86caa8f7ccbb62d7de5ae45d34cd5
dean <deanms@qq.com>
2021-06-16 20:00:19 +0800

    Add Test for Task 3 [Check if createTrade() of tradeRepository called when execute createTrade() of TradeService, and the trade id is same as the one returned by tradeRepository]

--- src/test/java/com/tw/comprehensive/TradingServiceTest.java
+++ src/test/java/com/tw/comprehensive/TradingServiceTest.java
@@ -37,4 +37,22 @@ class TradingServiceTest {
         assertEquals(trade.getDescription(), tradeRepository.findById(anyLong()).getDescription());
         assertEquals(trade.getReference(), tradeRepository.findById(anyLong()).getReference());
     }
+
+    @Test
+    void should_trade_repo_been_call_and_trade_id_is_same_when_call_create_trade() {
+        //given
+        TradeRepository tradeRepository = mock(TradeRepository.class);
+        AuditService auditService = mock(AuditService.class);
+        Trade trade = mock(Trade.class);
+        TradingService tradingService = new TradingService(tradeRepository, auditService);
+
+        //when
+        Long id = tradingService.createTrade(trade);
+
+        //then
+        //has been call
+        verify(tradeRepository, times(1)).createTrade(trade);
+        //id is same
+        assertEquals(tradeRepository.createTrade(trade), id);
+    }
 }
